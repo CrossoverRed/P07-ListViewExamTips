059a845a4561a5afb9178d29d30bb405a7810bdb
22015010 <22015010@myrp.edu.sg>
2023-07-10 21:10:18 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c326/id22015010/p07_listviewexamtips/MainActivity.java
@@ -0,0 +1,36 @@
+package sg.edu.rp.c326.id22015010.p07_listviewexamtips;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+ListView lvExamTips;
+String[] examTipsArray;
+ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvExamTips= findViewById(R.id.listViewExamTips);
+
+        examTipsArray=new String[5];
+        examTipsArray[0]="Don't just read the code, code it as much as possible" +
+                " during each practical session.";
+        examTipsArray[1]="Seek help from the lecturer ASAP, " +
+                "don't wait till you lost in the jungle.";
+        examTipsArray[2]="Prepare your template source code for each topic.";
+        examTipsArray[3]="Create a few empty Android projects to speed up your coding during the exam.";
+        examTipsArray[4]="Ensure that your Android Studio is up and running before the exam.";
+        //This one below will then not need ArrayAdapter<String> adapter; above the @Override
+        //ArrayAdapter adapter = new ArrayAdapter<>
+        //        (this, android.R.layout.simple_list_item_1, examTipsArray);
+        //lvExamTips.setAdapter(adapter);
+
+        //This one is used:
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, examTipsArray);
+        lvExamTips.setAdapter(adapter);
+    }
+}
